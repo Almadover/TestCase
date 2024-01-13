faebde43ab364d974433e6bbbc632aaea1effe83
Sergei <serge-koleso@yandex.ru>
2024-01-13 13:24:52 +0300

    initial commit

---
+++ test_ya_disk.py
@@ -0,0 +1,9 @@
+from ya_disk import *
+from unittest import TestCase
+
+class TestYandex(TestCase):
+    def test_get_upload(self):
+        self.assertEqual(akk_ya.get_upload('test'), 201, "folder created")
+
+    def test_get_upload_fail(self):
+        self.assertEqual(akk_ya.get_upload('test'), 201, "folder not created")
